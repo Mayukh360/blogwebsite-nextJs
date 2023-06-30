c376596ee14fff35f410b8a94db3e7a16ed52585
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-30 21:06:01 +0530

    Navbar Done

---
+++ src/app/components/blogCard/BlogCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './BlogCard.module.css'
+
+export default function BlogCard() {
+  return (
+    <div>BlogCard</div>
+  )
+}
---
+++ src/app/lib/data.js
@@ -0,0 +1,32 @@
+import img1 from '../../../public/asset/nature1.avif'
+import img2 from '../../../public/asset/nature2.avif'
+import img3 from '../../../public/asset/nature3.avif'
+import img4 from '../../../public/asset/nature4.avif'
+
+
+export const blogs = [
+    {
+        title: 'Blog 1',
+        desc: 'lorem ipsum',
+        img: img1,
+        authorId: 'someauthorid',
+    },
+    {
+        title: 'Blog 2',
+        desc: 'lorem ipsumsdds',
+        img: img2,
+        authorId: 'someauthorid',
+    },
+    {
+        title: 'Blog 3',
+        desc: 'lorem ipsumsddsdssdsd',
+        img: img3,
+        authorId: 'someauthorid',
+    },
+    {
+        title: 'Blog 4',
+        desc: 'lorem ipsumdsdsdssdsd',
+        img: img4,
+        authorId: 'someauthorid',
+    },
+]
