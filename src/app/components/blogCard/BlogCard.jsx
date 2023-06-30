8e56e268178af6cce1da2596c190960c3d5ebdd4
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-30 21:43:42 +0530

    Footer And Homepage Added

--- src/app/components/blogCard/BlogCard.jsx
+++ src/app/components/blogCard/BlogCard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import classes from './BlogCard.module.css'
+import React from "react";
+import classes from "./BlogCard.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { AiFillLike } from "react-icons/ai";
+import { AiOutlineLike } from "react-icons/ai";
 
-export default function BlogCard() {
+export default function BlogCard({ blog: { title, desc, img } }) {
+  const isLiked = true;
   return (
-    <div>BlogCard</div>
-  )
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <Link className={classes.imgContainer} href={`/`}>
+          <Image src={img} width="350" height="350" />
+        </Link>
+        <div className={classes.blogData}>
+          <div className={classes.left}>
+            <h3 className={title}></h3>
+            <p>{desc}</p>
+            <div className={classes.right}>
+              {12}{" "}
+              {isLiked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -3,6 +3,29 @@ import classes from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={classes.footer}>
+    <div className={classes.wrapper}>
+      <div className={classes.col}>
+        <h2>About the App</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Voluptates velit fuga perspiciatis itaque iste, aliquid dignissimos voluptate modi,
+          tempore assumenda adipisci dolor hic atque quod consequuntur cupiditate. Quasi, nobis veritatis!
+        </p>
+      </div>
+      <div className={classes.col}>
+        <h2>Contacts</h2>
+        <span>Phone +123 456 789</span>
+        <span>YouTube: TechyFreak</span>
+        <span>GitHub: Mayukh360</span>
+      </div>
+      <div className={classes.col}>
+        <h2>Location</h2>
+        <span>Continent: Asia</span>
+        <span>Country: India</span>
+        <span>Current Location: Kolkata</span>
+      </div>
+    </div>
+  </footer>
   )
 }
